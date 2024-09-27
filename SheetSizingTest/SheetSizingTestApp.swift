cb945549ac4c62e23d2fe46fa877618f16ce72d0
Donald Anderson <da@sysnet.co.uk>
2024-09-27 09:30:33 +0100

    Initial Commit

---
+++ SheetSizingTest/SheetSizingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetSizingTestApp.swift
+//  SheetSizingTest
+//
+//  Created by Donald Anderson on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SheetSizingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
